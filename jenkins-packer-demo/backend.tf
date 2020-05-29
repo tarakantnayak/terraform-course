bcbb3b06c027fd86479beb2bc9561fc39383fa96
tarakantnayak <55343071+tarakantnayak@users.noreply.github.com>
2020-05-29 16:21:03 +0530

    uncommented the code

--- jenkins-packer-demo/backend.tf
+++ jenkins-packer-demo/backend.tf
@@ -1,7 +1,7 @@
-#terraform {
-#  backend "s3" {
-#    bucket = "terraform-state-qul4r4ux"
-#    key    = "terraform.tfstate"
-#    region = "eu-west-1"
-#  }
-#}
+terraform {
+  backend "s3" {
+    bucket = "terraform-state-qul4r4ux"
+    key    = "terraform.tfstate"
+    region = "eu-west-1"
+  }
+}
